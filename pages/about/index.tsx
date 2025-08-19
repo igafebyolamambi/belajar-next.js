779ed781e4aded81d939963076b4b140519708dc
igafebyolamambi <ghabyfebyola@gmail.com>
2025-08-20 04:56:31 +0700

    tugas hari-12

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,3 +1,19 @@
+/*import Image from "next/image"*/
+
+
+/*export default function About() {
+    return <div>
+        <h1>About Page</h1>
+        <Image src="/Tulip 1.jpg" width="1000" height="800" size="100vw"/>
+    </div>
+}*/
+
+
+import ImgHight from '@/assets/Tulip 1.jpg'
+
 export default function About() {
-    return <div>About Page</div>
+    return <div>
+        <h1>About Page</h1>
+        <img {... ImgHight}/>
+    </div>
 }
